efbfead7bd914fd53eb8c4b1420102cade5cc384
Anja-Klassen <anja.klassen@posteo.de>
2024-09-30 09:49:46 +0200

    add solution

--- sessions/js-functions/progress-bar/js/index.js
+++ sessions/js-functions/progress-bar/js/index.js
@@ -21,4 +21,14 @@ For that:
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+  const scrollYPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const webpageHeight = document.body.clientHeight;
+  return (scrollYPosition / (webpageHeight - windowHeight)) * 100;
+}
+
+document.addEventListener("scroll", () => {
+  const scrollPositionInPercentage = calculateScrollPercentage();
+  progressBar.style.width = scrollPositionInPercentage + "%";
+});
